85a2b8766c8e498ab0f0b5f7910591080568be08
Julius van der Vaart <studio@juliusvaart.com>
2016-03-22 11:39:01 +0100

    Update voor D7

--- sharing.php
+++ sharing.php
@@ -6,16 +6,24 @@
   <ul>
     <li class="title">Delen op</li>
     <li> 
-      <a class="twitter" target="blank_" href="http://twitter.com/home?status=<?php print drupal_get_title(); ?> <?php print url(current_path(), array('absolute' => true)); ?>">Twitter</a>
+      <a class="twitter" target="blank_" rel="nofollow" href="http://twitter.com/home?status=<?php print drupal_get_title(); ?> <?php print url(current_path(), array('absolute' => true)); ?>">Twitter</a>
     </li>
     <li>
-      <a class="facebook" target="blank_" href="https://www.facebook.com/sharer/sharer.php?u=<?php print url(current_path(), array('absolute' => true)); ?>" target="_blank">Facebook</a>
+      <a class="facebook" target="blank_" rel="nofollow" href="https://www.facebook.com/sharer/sharer.php?u=<?php print url(current_path(), array('absolute' => true)); ?>" target="_blank">Facebook</a>
     </li>
     <li>
-      <a class="linkedin" target="blank_" href="http://www.linkedin.com/shareArticle?mini=true&url=<?php print url(current_path(), array('absolute' => true)); ?>&title=<?php print drupal_get_title(); ?>" target="_blank">LinkedIn</a>
+      <a class="linkedin" target="blank_" rel="nofollow" href="http://www.linkedin.com/shareArticle?mini=true&url=<?php print url(current_path(), array('absolute' => true)); ?>&title=<?php print drupal_get_title(); ?>" target="_blank">LinkedIn</a>
     </li>
     <li>
-      <a class="email" target="blank_" href="mailto:?subject=<?php print drupal_get_title(); ?>&body=Ik wil dit artikel met je delen: <?php print url(current_path(), array('absolute' => true)); ?>">E-mail</a>
+      <a class="email" target="blank_" rel="nofollow" href="mailto:?subject=<?php print drupal_get_title(); ?>&body=Ik wil dit artikel met je delen: <?php print url(current_path(), array('absolute' => true)); ?>">E-mail</a>
     </li>
   </ul>
 </div>
+
+<!-- V2 With Drupal 7 t() -->
+<div class="share">
+  <p>
+    <?php print t('Share this article on'); ?>: <a class="twitter" target="blank_" rel="nofollow" href="http://twitter.com/home?status=<?php print drupal_get_title(); ?> <?php print url(current_path(), array('absolute' => true)); ?>">twitter</a>, <a class="facebook" target="blank_" rel="nofollow" href="https://www.facebook.com/sharer/sharer.php?u=<?php print url(current_path(), array('absolute' => true)); ?>" target="_blank">facebook</a>, <a class="linkedin" target="blank_" rel="nofollow" href="http://www.linkedin.com/shareArticle?mini=true&url=<?php print url(current_path(), array('absolute' => true)); ?>&title=<?php print drupal_get_title(); ?>" target="_blank">linkedin</a> <?php print t('or via'); ?> <a class="email" target="blank_" rel="nofollow" href="mailto:?subject=<?php print drupal_get_title(); ?>&body=<?php print t('I\'d like to share this with you:'); ?> <?php print url(current_path(), array('absolute' => true)); ?>">e-mail</a>
+  </p>
+</div>
+  
